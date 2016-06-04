10d5d194b6509f61fa6a5ebc078be2453d7c50fc
Thirunavukkarasu Muthuswamy <Thirunavukkarasu.Muthuswamy.ap@gmail.com>
2016-06-04 17:04:48 +0530

    third commit

--- app.js
+++
@@ -1,7 +0,0 @@
-var trackion = angular.module('Trackion',[]);
-
-trackion.controller('HomeCtrl', ['$scope','$http', function ($scope,$http) {
-	$http.get('data/ideas.json').then(function(response){
-		$scope.listOfIdeas = response.data;
-	})
-}]);
