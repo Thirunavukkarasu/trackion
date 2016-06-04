b8fde76642af9cf25be890edfa4588d495b40cb3
Thirunavukkarasu Muthuswamy <Thirunavukkarasu.Muthuswamy.ap@gmail.com>
2016-06-05 00:55:24 +0530

    third commit

--- app/app.js
+++ app/app.js
@@ -7,6 +7,25 @@ angular.module('Trackion',[
 
 .config(['$routeProvider', function ($routeProvider) {
      $routeProvider.otherwise({redirectTo:'/'}); 
-}]);
+}])
+
+.directive('a', function() {
+    return {
+        restrict: 'E',
+        link: function(scope, elem, attrs) {
+            if(attrs.ngClick || attrs.href === '' || attrs.href === '#'){
+                elem.on('click', function(e){
+                    e.preventDefault();
+                });
+            }
+        }
+   };
+})
+
+.controller('HeaderController', function ($scope,$location) {
+	  $scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
+});
 
 
